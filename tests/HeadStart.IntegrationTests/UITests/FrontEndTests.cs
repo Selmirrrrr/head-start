ff219e6c9268e73766eef863e682f79638396cb8
Selmir <git@hajruli.ch>
2025-08-24 18:28:24 +0200

    tests: ci test run fix try #6

--- tests/HeadStart.IntegrationTests/UITests/FrontEndTests.cs
+++ tests/HeadStart.IntegrationTests/UITests/FrontEndTests.cs
@@ -7,6 +7,12 @@ namespace HeadStart.IntegrationTests.UITests;
 [ClassDataSource<PlaywrightDataClass>]
 public class FrontEndTests(PlaywrightDataClass playwrightDataClass) : PageTest
 {
+    protected override BrowserNewContextOptions ContextOptions()
+    {
+        var options = base.ContextOptions();
+        options.IgnoreHTTPSErrors = true;
+        return options;
+    }
     [Test]
     public async Task TestWebAppHomePageAsync()
     {
