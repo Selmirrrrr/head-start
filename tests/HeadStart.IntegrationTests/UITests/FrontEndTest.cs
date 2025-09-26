84fe8fadd3fd7a41cb0510cdc82539e8694ed181
Selmir <git@hajruli.ch>
2025-09-27 00:13:53 +0200

    tests: fixed LoginTestAsync

--- tests/HeadStart.IntegrationTests/UITests/FrontEndTest.cs
+++ tests/HeadStart.IntegrationTests/UITests/FrontEndTest.cs
@@ -18,32 +18,28 @@ public class FrontEndTests(AspireDataClass playwrightDataClass) : PlaywrightTest
         await Assert.That(title).IsEqualTo("Claimly");
     }
 
-    // [Test]
-    // public async Task LoginTestAsync()
-    // {
-    //     await Page.GotoAsync(playwrightDataClass.BaseUrl.ToString());
-    //
-    //     await Page.WaitForURLAsync($"{playwrightDataClass.KeycloakUrl}**", new() { Timeout = 10000 });
-    //
-    //     var title = await Page.TitleAsync();
-    //
-    //     await Assert.That(title).IsEqualTo("Se connecter à HeadStart");
-    //
-    //     await Page.WaitForSelectorAsync("input[name='username']", new() { Timeout = 10000 });
-    //
-    //     // Fill in the login credentials
-    //     await Page.FillAsync("input[name='username']", "user@example.com");
-    //     await Page.FillAsync("input[name='password']", "user");
-    //
-    //     // Submit the login form
-    //     await Page.Locator("#kc-login").ClickAsync();
-    //     // Wait to be redirected back to the main application
-    //     await Page.GetByRole(AriaRole.Heading, new() { Name = "Claimly" }).WaitForAsync();
-    //
-    //     // Verifiy that we see the logout button
-    //     await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "Logout" })).ToBeVisibleAsync();
-    //     await Expect(Page.GetByText("Default User", new() { Exact = true })).ToBeVisibleAsync();
-    // }
+    [Test]
+    public async Task LoginTestAsync()
+    {
+        await Page.GotoAsync(playwrightDataClass.BaseUrl.ToString());
+
+        await Page.WaitForURLAsync($"{playwrightDataClass.KeycloakUrl}**", new() { Timeout = 10000 });
+
+        await Page.WaitForSelectorAsync("input[name='username']", new() { Timeout = 10000 });
+
+        // Fill in the login credentials
+        await Page.FillAsync("input[name='username']", "user1@example.com");
+        await Page.FillAsync("input[name='password']", "user1");
+
+        // Submit the login form
+        await Page.Locator("#kc-login").ClickAsync();
+        // Wait to be redirected back to the main application
+        await Page.GetByRole(AriaRole.Heading, new() { Name = "Claimly" }).First.WaitForAsync();
+
+        // Verifiy that we see the users name in the user card button
+        await Expect(Page.Locator(".mud-typography.mud-typography-body2"))
+            .ToContainTextAsync("FirstName1");
+    }
     //
     // [Test]
     // public async Task LogoutTestsAsync()
